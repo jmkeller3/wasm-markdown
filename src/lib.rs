0a63c53ed924b317c376fd7568fa4af7c4be76c0
John Keller <jmkeller3@gmail.com>
2021-01-25 13:42:26 -0700

    working markdown in js and wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use pulldown_cmark::{Parser, Options, html};
+
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +11,16 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+pub fn render(markdown_input: &str) -> String {
+        
+    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+    // and we therefore must enable it explicitly.
+    let options = Options::empty();
+    let parser = Parser::new_ext(markdown_input, options);
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, markdown!");
+    // Write to String buffer.
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    html_output
 }
